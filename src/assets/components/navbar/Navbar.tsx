96a2b0514e0e902a0e57598f08503ccf73685b98
YancaDantas <yanca.d@live.com>
2024-08-14 15:18:23 -0300

    Conectando com API - Deletar Tema

--- src/assets/components/navbar/Navbar.tsx
+++ src/assets/components/navbar/Navbar.tsx
@@ -1,36 +1,35 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../../contexts/AuthContext'
 
 function Navbar() {
+  let navigate = useNavigate()
+  const { handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
   return (
     <div className='w-full bg-red-900 text-white flex justify-center py-4'>
-      <div className="container flex justify-between text-lg items-center">
-        <div className='text-2xl font-bold uppercase'>
-          <Link to="/" className="hover:underline">Blog Pessoal</Link>
-        </div>
+      <div className='max-w-screen-xl w-full flex justify-between text-lg px-4'>
+        <Link to='/home' className='text-2xl font-bold uppercase'>Blogger</Link>
 
-        <div className='flex gap-4 items-center'>
-          <Link to="/postagens" className='hover:underline'>Postagens</Link>
-          <Link to="/temas" className='hover:underline'>Temas</Link>
-          <Link to="/cadastrar-tema" className='hover:underline'>Cadastrar tema</Link>
-          <Link to="/perfil" className='hover:underline'>Perfil</Link>
-          <Link to="/sair" className='hover:underline'>Sair</Link>
-
-          <div className="flex flex-grow justify-end">
-            <input
-              type="text"
-              placeholder="Pesquisar..."
-              className="text-black bg-white px-4 py-2 rounded-full"
-            />
-          </div>
+        <div className='flex gap-4'>
+            <div className='hover:underline'>Postagens</div>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+              <div className='hover:underline'>Perfil</div>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Navbar;
-
+export default Navbar
 
         
   
