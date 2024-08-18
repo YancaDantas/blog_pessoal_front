a6582fac1f0b47929d691b445f2f7ea418685cbc
YancaDantas <yanca.d@live.com>
2024-08-18 18:25:20 -0300

    Criar, editar e deletar postagens

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,9 @@ import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './assets/components/temas/listaTemas/ListaTemas';
 import FormularioTema from './assets/components/temas/formularioTema/FormularioTema';
 import DeletarTema from './assets/components/temas/deletarTema/DeletarTema';
-
+import ListaPostagens from './assets/components/postagens/listaPostagem/ListaPostagem';
+import FormularioPostagem from './assets/components/postagens/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './assets/components/postagens/deletarPostagem/DeletarPostagem';
 
 function App() {
   return (
@@ -29,6 +31,9 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,41 +1,35 @@
 import "./Home.css";
+import React from 'react';
 import Blog from '../../assets/blog.jpg';
-import React, { useContext } from 'react';
-import { UserContext } from '../../contexts/UserContexts';
-import { Link } from 'react-router-dom';
+import './Home.css';
+import ListaPostagens from '../../assets/components/postagens/listaPostagem/ListaPostagem';
+import ModalPostagem from '../../assets/components/postagens/modalPostagem/ModalPostagem';
+
 
 function Home() {
-  const { nome } = useContext(UserContext);
-
-  return (
-    <div className="flex flex-col items-center bg-beige-800 min-h-[83vh] py-4 px-8">
-      <div className="flex flex-col lg:flex-row items-center justify-between w-full max-w-7xl">
-        {/* Seção da Imagem */}
-        <div className="flex-1 flex items-center justify-center lg:justify-start">
-          <img src={Blog} alt="Imagem do blog" className="w-full max-w-md h-auto" />
-        </div>
-
-        {/* Seção de Informações */}
-        <div className="flex-1 flex flex-col items-center lg:items-start text-center lg:text-left lg:ml-8">
-          <div className="mb-8">
-            <h1 className="text-5xl color-texts mb-4">Blog Pessoal</h1>
-            <h2 className="color-texts mb-8">Seu espaço para Criatividade!</h2>
-          </div>
-          
-          <div className="flex flex-col items-center lg:items-start">
-            <h2 className="text-slate-900 text-5xl mb-4">Seja bem-vindo!</h2>
-            <Link
-              to="/login"
-              className="rounded bg-red-900 hover:bg-indigo-800 text-white py-2 px-4 flex justify-center"
-            >
-              Voltar
-            </Link>
+    return (
+        <>
+        <div className="bg-red-500 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Bem vindo!</h2>
+              <p className='text-xl'>Seu espaço para criatividade!</p>
+  
+              <div className="flex justify-around gap-4">
+              <ModalPostagem />
+                <button className='rounded bg-white text-red-600 py-2 px-4'>Ver postagens</button>
+              </div>
+            </div>
+  
+            <div className="flex justify-center ">
+              <img src={Blog} alt="" className='w-2/3' />
+      
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+        <ListaPostagens />
+      </>
+    );
 }
 
 export default Home;
-
